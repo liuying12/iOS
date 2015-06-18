4030d740eb4b26e2c14028135296b43a2f519f5b
tim9liu9 <doinone@163.com>
2015-06-18 23:22:08 +0800

    第一次提交代码

---
+++ 150618/150618-oc类/150618-oc类/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  150618-oc类
+//
+//  Created by timliu on 15/6/18.
+//  Copyright (c) 2015年 ibm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
